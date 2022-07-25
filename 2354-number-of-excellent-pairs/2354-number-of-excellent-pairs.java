654c2ef57cae547b4c9ce06af828eea650878816
harikrishnarudra <56203012+harikrishnarudra@users.noreply.github.com>
2022-07-25 23:32:05 +0530

    Time: 95 ms (75.00%), Space: 101.3 MB (25.00%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.java
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.java
@@ -3,11 +3,13 @@ class Solution {
         long res = 0;
         int[] bitCnt = new int[32];
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++)
-            set.add(nums[i]);
-        for(int a:set)
-            bitCnt[Integer.bitCount(a)]++;
-
+        for (int i = 0; i < nums.length; i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                bitCnt[Integer.bitCount(nums[i])]++;
+            }
+        }
+        
         for(int i=0;i<32;i++){
             for(int j=0;j<32;j++){
                 if(i+j >=k){
