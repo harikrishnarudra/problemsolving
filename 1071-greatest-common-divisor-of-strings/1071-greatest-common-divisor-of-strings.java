a28b8e777539a40de48d2b6aba3f7aa07ce33c17
harikrishnarudra <56203012+harikrishnarudra@users.noreply.github.com>
2022-10-28 10:20:07 +0530

    Time: 14 ms (16.20%), Space: 47.7 MB (9.74%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,23 +1,17 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        int s1Leng = str1.length();
-        int s2Leng = str2.length();
         Set<Character> visitedChars = new HashSet<>();
-        String subStr = s1Leng > s2Leng ? str2 : str1;
+        String subStr = str1.length() > str2.length() ? str2 : str1;
         int len = subStr.length();
         String maxSubString = "";
-        int i = 0;
-        char[] s1 = str1.toCharArray();
-        char[] s2 = str2.toCharArray();
-        if (subStr.length() > 0 && replaceableString(subStr, str1) && replaceableString(subStr, str2)) {
+        if (len > 0 && replaceableString(subStr, str1) && replaceableString(subStr, str2)) {
             return subStr;
         }
-
         for(int j=0;j<len;j++){
-            if (!visitedChars.contains(s1[j]) && s1[j] == s2[j]) {
-                visitedChars.add(s1[j]);
+            if (!visitedChars.contains(str1.charAt(j)) && str1.charAt(j) == str2.charAt(j)) {
+                visitedChars.add(str1.charAt(j));
             } else {
-                String subString = str1.substring(i, j - i);
+                String subString = str1.substring(0, j);
                 if (subString.length() > 0 && replaceableString(subString, str1) && replaceableString(subString, str2)) {
                     if (subString.length() > maxSubString.length()) {
                         maxSubString = subString;
