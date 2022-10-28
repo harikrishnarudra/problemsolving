e0bcfdd2ffb9f68f19bf43f8e1daa454e40168de
harikrishnarudra <56203012+harikrishnarudra@users.noreply.github.com>
2022-10-28 10:28:12 +0530

    Time: 1 ms (94.66%), Space: 42.8 MB (48.31%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,40 +1,11 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        Set<Character> visitedChars = new HashSet<>();
-        String subStr = str1.length() > str2.length() ? str2 : str1;
-        int len = subStr.length();
-        String maxSubString = "";
-        if (len > 0 && replaceableString(subStr, str1) && replaceableString(subStr, str2)) {
-            return subStr;
-        }
-        for(int j=0;j<len;j++){
-            if (!visitedChars.contains(str1.charAt(j)) && str1.charAt(j) == str2.charAt(j)) {
-                visitedChars.add(str1.charAt(j));
-            } else {
-                String subString = str1.substring(0, j);
-                if (subString.length() > 0 && replaceableString(subString, str1) && replaceableString(subString, str2)) {
-                    if (subString.length() > maxSubString.length()) {
-                        maxSubString = subString;
-                    }
-                }
-            }
-        }
-        return maxSubString;
+        if(!(str1+str2).equals(str2+str1)) return "";
+        int len = gcd(str1.length(), str2.length());
+        return str2.substring(0, len);
     }
-
-    private boolean replaceableString(String subStr, String givenString){
-        int len = givenString.length();
-        int subLen = subStr.length();
-        if(len%subLen == 0){
-            int n = len/subLen;
-            StringBuilder stringBuilder = new StringBuilder();
-            int k=0;
-            while(k < n){
-                stringBuilder.append(subStr);
-                k++;
-            }
-            return stringBuilder.toString().equalsIgnoreCase(givenString);
-        }
-        return false;
+    private static int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b, a%b);
     }
 }
